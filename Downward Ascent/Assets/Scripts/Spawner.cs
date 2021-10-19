82492d32445a86f618e3035a03f9599df2833e60
MatthewLiv <s-livingstonm@bsd405.org>
2021-10-19 11:39:26 -0700

    yay

---
+++ Downward Ascent/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] landFabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Random r = new Random();
+        //int num = r.Next(0, 1);
+        // Instantiate(landFabs[0], new Vector3(0, 0, 0), landFabs[0].transform.rotation);
+
+        int num = Random.Range(0, 2);
+        Instantiate(landFabs[num], new Vector3(0, 0, 0), landFabs[num].transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
