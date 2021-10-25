df853261e9baa6e4e0073b70939996762d1f9161
MatthewLiv <s-livingstonm@bsd405.org>
2021-10-25 10:45:03 -0700

    ugh

--- Downward Ascent/Assets/Scripts/Spawner.cs
+++ Downward Ascent/Assets/Scripts/Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject[] landFabs;
+    public GameObject[] Starters;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,16 +12,9 @@ public class Spawner : MonoBehaviour
         //int num = r.Next(0, 1);
         // Instantiate(landFabs[0], new Vector3(0, 0, 0), landFabs[0].transform.rotation);
 
-        int num = Random.Range(0, 1);
-        Instantiate(landFabs[num], new Vector3(0, 0, 0), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3((float) 15.5, 0, 0), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3((float)-15.5, 0, 0), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3(0, 0, (float)15.5), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3(0, 0, (float)-15.5), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3((float)15.5, 0, (float)15.5), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3((float)15.5, 0, (float)-15.5), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3((float)-15.5, 0, (float)-15.5), landFabs[num].transform.rotation);
-        Instantiate(landFabs[num], new Vector3((float)-15.5, 0, (float)15.5), landFabs[num].transform.rotation);
+        int num = Random.Range(1, 2);
+        Instantiate(Starters[num], new Vector3(0, 0, 0), Starters[num].transform.rotation);
+       
     }
 
     // Update is called once per frame
