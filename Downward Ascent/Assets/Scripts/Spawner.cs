3a83416cbf10477e31fda8afb3641f9dbbea2b29
MatthewLiv <s-livingstonm@bsd405.org>
2021-10-31 17:23:29 -0700

    ruussdte

--- Downward Ascent/Assets/Scripts/Spawner.cs
+++ Downward Ascent/Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] Starters;
     public GameObject yoyoyo;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,11 @@ public class Spawner : MonoBehaviour
         int num = Random.Range(0, 1);
         Vector3 position = Vector3.zero;
         GameObject g = Instantiate(Starters[num], position, Starters[num].transform.rotation);
-        g.localPosition = new Vector3(-1 * g.transform.TransformPoint(0).x(), -1 * g.transform.TransformPoint(0).y(), -1 * g.transform.TransformPoint(0).z())
+        
+        Debug.LogError(g.transform.position);
+
+        Debug.LogError(yoyoyo.transform.position + "ookeeboogey");
+        
        // Vector3 position2 = new Vector3(0, 0, 0);
        //GameObject THEg = Instantiate(yoyoyo, position2, yoyoyo.transform.rotation);
        // THEg.transform.position = position2;
