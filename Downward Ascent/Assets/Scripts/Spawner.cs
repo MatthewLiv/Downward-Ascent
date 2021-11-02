b66accb24abf4ed7f12f62429893e5781edc07e2
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-01 17:03:02 -0700

    YEEEEES

--- Downward Ascent/Assets/Scripts/Spawner.cs
+++ Downward Ascent/Assets/Scripts/Spawner.cs
@@ -17,10 +17,8 @@ public class Spawner : MonoBehaviour
         int num = Random.Range(0, 1);
         Vector3 position = Vector3.zero;
         GameObject g = Instantiate(Starters[num], position, Starters[num].transform.rotation);
+        Instantiate(yoyoyo, new Vector3(15, 0, 0), yoyoyo.transform.rotation);
         
-        Debug.LogError(g.transform.position);
-
-        Debug.LogError(yoyoyo.transform.position + "ookeeboogey");
         
        // Vector3 position2 = new Vector3(0, 0, 0);
        //GameObject THEg = Instantiate(yoyoyo, position2, yoyoyo.transform.rotation);
