656e32cbf865c73715ea612b379d0557433aa089
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-03 15:37:02 -0700

    Aaron Burr, sir

--- Downward Ascent/Assets/Scripts/Spawner.cs
+++ Downward Ascent/Assets/Scripts/Spawner.cs
@@ -15,11 +15,12 @@ public class Spawner : MonoBehaviour
         //int num = r.Next(0, 1);
         // Instantiate(landFabs[0], new Vector3(0, 0, 0), landFabs[0].transform.rotation);
 
-        int num = Random.Range(0, 1);
+        int num = Random.Range(0, 5);
         Vector3 position = Vector3.zero;
         GameObject g = Instantiate(Starters[num], position, Starters[num].transform.rotation);
+        //placeR(g);
 
-        int random = Random.Range(0, 2);
+     /*   int random = Random.Range(0, 2);
         if (random == 1)
         {
             Instantiate(yoyoyo, new Vector3(16, 0, 0), yoyoyo.transform.rotation);
@@ -29,8 +30,36 @@ public class Spawner : MonoBehaviour
             Instantiate(yoyoyo, new Vector3(-16, 0, 0), yoyoyo.transform.rotation);
         }
 
-        Debug.LogError(path1[0]);
+        Debug.LogError(path1[0]);*/
       
+      
+
+
+    }
+
+    private void placeR(GameObject g)
+    {
+        int num = Random.Range(0, 4);
+        if (num == 0)
+        {
+            return;
+        }
+        /*else if (num == 1)
+        {
+            g.transform.Rotate(Vector3.up, 90);
+            g.transform.Translate(new Vector3(16, 0, 0));
+        }
+        else if (num == 2)
+        {
+            g.transform.Rotate(Vector3.up, 180);
+            g.transform.Translate(new Vector3(16, 0, 16));
+        }*/
+        else
+        {
+            g.transform.Rotate(Vector3.up, 270);
+            // g.transform.Translate(new Vector3(0, 0, 16));
+            g.transform.SetPositionAndRotation(new Vector3(15, 0, 0), new Quaternion(0, 270, 0, 1));  
+        }
 
 
 
