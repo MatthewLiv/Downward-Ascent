d4ba9bd1ffa76c3fcd07ab1313bd94630278a6e6
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-04 10:27:03 -0700

    A tad of a g

--- Downward Ascent/Assets/Scripts/Spawner.cs
+++ Downward Ascent/Assets/Scripts/Spawner.cs
@@ -15,12 +15,12 @@ public class Spawner : MonoBehaviour
         //int num = r.Next(0, 1);
         // Instantiate(landFabs[0], new Vector3(0, 0, 0), landFabs[0].transform.rotation);
 
-        int num = Random.Range(0, 5);
+        int num = Random.Range(1, 2);
         Vector3 position = Vector3.zero;
         GameObject g = Instantiate(Starters[num], position, Starters[num].transform.rotation);
-        //placeR(g);
+        placeR(g);
 
-     /*   int random = Random.Range(0, 2);
+        /*int random = Random.Range(0, 2);
         if (random == 1)
         {
             Instantiate(yoyoyo, new Vector3(16, 0, 0), yoyoyo.transform.rotation);
@@ -30,8 +30,8 @@ public class Spawner : MonoBehaviour
             Instantiate(yoyoyo, new Vector3(-16, 0, 0), yoyoyo.transform.rotation);
         }
 
-        Debug.LogError(path1[0]);*/
-      
+        Debug.LogError(path1[0]);
+      */
       
 
 
@@ -44,23 +44,24 @@ public class Spawner : MonoBehaviour
         {
             return;
         }
-        /*else if (num == 1)
+        else if (num == 1)
         {
             g.transform.Rotate(Vector3.up, 90);
-            g.transform.Translate(new Vector3(16, 0, 0));
+            g.transform.Translate(new Vector3(-15, 0, 0));
         }
         else if (num == 2)
         {
             g.transform.Rotate(Vector3.up, 180);
-            g.transform.Translate(new Vector3(16, 0, 16));
-        }*/
+            g.transform.Translate(new Vector3(-15, 0, -15));
+        }
         else
         {
             g.transform.Rotate(Vector3.up, 270);
-            // g.transform.Translate(new Vector3(0, 0, 16));
-            g.transform.SetPositionAndRotation(new Vector3(15, 0, 0), new Quaternion(0, 270, 0, 1));  
+            g.transform.Translate(new Vector3(0, 0, -15));
+       // g.transform.Position(new Vector3(15, 0, 0));
+           // g.transform.SetPositionAndRotation(new Vector3(15, 0, 0), new Quaternion(0, 180, 0, 1));  
         }
-
+        
 
 
     }
