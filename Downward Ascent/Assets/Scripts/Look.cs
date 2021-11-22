e26456bc9b7e29bfa5ee174836c9c51b23773b8b
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-22 10:38:36 -0800

    can you hear the thunder

---
+++ Downward Ascent/Assets/Scripts/Look.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+
+
+{
+    public Vector3 Vec;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         yaw += speedH * Input.GetAxis("Mouse X");
+         pitch -= speedV * Input.GetAxis("Mouse Y");
+
+         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        
+        Vec = transform.localPosition;
+        Vec.x = Vec.x + Input.GetAxis("Mouse X") * Time.deltaTime * 20;
+        Vec.z = Vec.z + Input.GetAxis("Mouse Y") * Time.deltaTime * 20;
+        transform.localPosition = Vec;
+
+
+
+    }
+}
