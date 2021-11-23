bbecbc7ecae65a9aab41864f6f1078b7ff53efc8
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-23 10:44:03 -0800

    this music is terrible

--- Downward Ascent/Assets/Scripts/Look.cs
+++ Downward Ascent/Assets/Scripts/Look.cs
@@ -13,7 +13,8 @@ public class Look : MonoBehaviour
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
-
+    public bool isJump;
+    private float yspot = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +37,26 @@ public class Look : MonoBehaviour
         transform.localPosition = Vec;
 
 
+        Rigidbody r = gameObject.GetComponent<Rigidbody>();
+
+
+
+        if (Input.GetKey("space") && !isJump)
+        {
+            isJump = true;
+            yspot = gameObject.transform.position.y;
+
+
+            //object.Rigidbody.AddForce(Vector3.up * 5);
+            
+            r.AddForce(Vector3.up * 150);
+            
+        }
+
+       
+
+        Debug.Log(yspot);
+        Debug.Log(isJump);
 
     }
 }
