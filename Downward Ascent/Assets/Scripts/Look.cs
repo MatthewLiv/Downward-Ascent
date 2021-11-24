e9e2a405f60e2a1c27c7072b29f4c3cd1d86274f
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-24 09:59:34 -0800

    turkey

--- Downward Ascent/Assets/Scripts/Look.cs
+++ Downward Ascent/Assets/Scripts/Look.cs
@@ -25,12 +25,36 @@ public class Look : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* yaw += speedH * Input.GetAxis("Mouse X");
+        int Xax = Input.GetAxis("MouseX");
+        if (Xax > 180 || Xax < -180)
+        {
+            yaw = 0;
+        }
+        else
+        {
+            yaw += speedH * Xax;
+        }
+
+        int Yax = Input.GetAxis("MouseY");
+        if (Yax > 180 || Yax < -180)
+        {
+            pitch = 0;
+        }
+        else
+        {
+            pitch -=peedH * Xax;
+        }
+
+
+
+
+        /*
+        yaw += speedH * Input.GetAxis("MouseX");
          pitch -= speedV * Input.GetAxis("Mouse Y");
 
          transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
         */
-        
+
         Vec = transform.localPosition;
         Vec.x = Vec.x + Input.GetAxis("Horizontal") * Time.deltaTime * 20;
         Vec.z = Vec.z + Input.GetAxis("Vertical") * Time.deltaTime * 20;
@@ -39,12 +63,12 @@ public class Look : MonoBehaviour
 
         Rigidbody r = gameObject.GetComponent<Rigidbody>();
 
-
+        yspot = gameObject.transform.position.y;
 
         if (Input.GetKey("space") && !isJump)
         {
             isJump = true;
-            yspot = gameObject.transform.position.y;
+            
 
 
             //object.Rigidbody.AddForce(Vector3.up * 5);
@@ -53,10 +77,18 @@ public class Look : MonoBehaviour
             
         }
 
+        else if (isJump && gameObject.transform.position.y == yspot)
+        {
+            isJump = false;
+        }
+        
+
+        
+
        
 
-        Debug.Log(yspot);
-        Debug.Log(isJump);
+       // Debug.Log(yspot);
+       // Debug.Log(isJump);
 
     }
 }
