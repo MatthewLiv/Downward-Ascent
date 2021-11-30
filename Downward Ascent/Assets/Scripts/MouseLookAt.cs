db72dc6e8d758fc13e0b3ab0c7f6f0331a363476
MatthewLiv <s-livingstonm@bsd405.org>
2021-11-30 10:45:02 -0800

    pulled the rug

--- Downward Ascent/Assets/Scripts/MouseLookAt.cs
+++ Downward Ascent/Assets/Scripts/MouseLookAt.cs
@@ -19,13 +19,17 @@ public class MouseLookAt : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        
         float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
 
         playerTransform.Rotate(Vector3.up * mouseX);
+       
 
-        xRotation = mouseY;
+        xRotation -= mouseY;
+
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
     }
 }
--- Downward Ascent/Assets/Scripts/Movement.cs
+++ Downward Ascent/Assets/Scripts/Movement.cs
@@ -8,6 +8,20 @@ public class Movement : MonoBehaviour
 
     public float speed = 10f;
 
+    bool isGrounded;
+
+    public LayerMask groundMask;
+
+    public Transform groundCheck;
+
+    float groundDistance = 0.4f;
+
+    Vector3 velocity;
+
+    public float gravity = -9.8f;
+
+    public float jumpheight = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +31,25 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+
+
+        controller.Move(velocity * Time.deltaTime);
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
