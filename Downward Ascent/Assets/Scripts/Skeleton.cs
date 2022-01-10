a64824f0e6d15a5c64e2d39007067d566c00559c
MatthewLiv <s-livingstonm@bsd405.org>
2022-01-10 10:43:21 -0800

    you did it william

--- Downward Ascent/Assets/Scripts/Skeleton.cs
+++ Downward Ascent/Assets/Scripts/Skeleton.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Skeleton : MonoBehaviour
-{/*
-    private bool Obstacle;
+{
+    
     
 
     private void Start()
@@ -17,107 +17,39 @@ public class Skeleton : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(0, 0, Time.deltaTime * 5);
+        transform.Translate(0, 0, Time.deltaTime *1);
 
-        checkObstacle();
-        int i = 0;
-        while (i == 0)
-        {
-                if (Obstacle)
-                {
-                    transform.Rotate(0, 90, 0);
-                i = 1;
-                }
-        }
-        
-        
-
-    }
-
-
-    private void checkObstacle()
-    {
         RaycastHit hit;
-        Vector3 p = transform.position;
-        Ray landingRay = new Ray(new Vector3(p.x, p.y + 0.5f, p.z), Vector3.forward);
-        Debug.DrawRay(new Vector3(p.x, p.y + 0.5f, p.z), Vector3.forward);
+        Ray landingRay = new Ray(transform.position, Vector3.down);
+
+
+        Debug.DrawRay(transform.position, Vector3.down, Color.blue);
+
+
+        
+
+
 
         if (Physics.Raycast(landingRay, out hit, 2))
         {
-            if (hit.collider.tag == "Ground")
+            Debug.Log("weel it hit");
+            if (hit.collider.tag == "Turner")
             {
-                Obstacle = true;
+                
+                transform.Rotate(0, 90, 0);
                 
 
             }
 
         }
+
     }
-
-
-
-
-    /*
-
- 
-
-    public LayerMask whatIsGround, whatIsPlayer;
-
-   
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private void Awake()
-    {
-        player = GameObject.Find("MyPersonController").transform;
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    private void Update()
-    {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
         
-
-       
-
-        if (!playerInSightRange && !playerInAttackRange)
-        {
-            Patroling();
-        }
-
-        if (playerInSightRange && !playerInAttackRange)
-        {
-            ChasePlayer();
-        }
-
-        if (playerInSightRange && playerInAttackRange)
-        {
-            attackPlayer();
-        }
-    }
-
-    private void Patroling()
-    {
-        
-    }
+  
 
    
     
-
-    private void ChasePlayer()
-    {
-        
-    }
-    private void AttackPlayer()
-    {
-        
-    }
-
-   
-    */
 
 
 }
