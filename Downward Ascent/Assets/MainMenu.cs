7e4c0b4c358e91181c311c1151872f9051778175
MatthewLiv <s-livingstonm@bsd405.org>
2022-05-04 15:57:00 -0700

    Blast that ship, OUT OF THE SKY

--- Downward Ascent/Assets/MainMenu.cs
+++ Downward Ascent/Assets/MainMenu.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("The Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
 }
